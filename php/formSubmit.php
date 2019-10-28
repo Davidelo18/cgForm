83abe3475993a77389b13b94ddd802d2ec8035a1
Dawid Kiełbik <dkielbik@e-point.pl>
2019-10-28 08:17:44 +0100

    Correct mail and info about price

--- php/formSubmit.php
+++ php/formSubmit.php
@@ -5,13 +5,15 @@ $email = $_POST['email'];
 $tel = $_POST['tel'];
 
 if(isset($name) && isset($lastname) && isset($email) && isset($tel)) {
-    $mailTo = 'dawiolo002@gmail.com';
+    $mailTo = 'marketing@copygeneral.pl, zaneta.szewczuk@copygeneral.pl';
     $subject = "Rejestracja do programu lojalnościowego";
     $message = 'Zarejestrowano'; // UZUPEŁNIĆ
 
     if (mail($mailTo, $subject, $message)) {
-        echo "Formularz został wysłany. Skontaktujemy się w najbliższym czasie.";
+        echo "Dziękujemy za rejestracje do programu rabatowego Copy General. Skontaktujemy się Panem/Panią w najbliższym czasie.";
+        echo "Zespół Copy General.";
     } else {
-        echo "Błąd przy wysyłaniu formularza. Spróbuj ponownie później";
+        echo "Nastąpił błąd podczas wysyłaniu formularza. Prosimy spróbować później.";
+        echo "Zespół Copy General";
     }
 }
